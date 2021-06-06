db922371e355cc45a19d32d0d886e654faff6f2a
김선영 <gimseon-yeong@gimseon-yeong-ui-MacBookAir.local>
2021-06-06 17:46:29 +0900

    test

--- Programmers_even.java
+++ Programmers_even.java
@@ -1,46 +1,45 @@
 import java.util.*;
 
 public class Programmers_even {
-    
+    public static int answer = 0;
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static boolean[] check = new boolean[7];
     public static void main(String[] args) {
         int result = solution("17");
         System.out.println("result :" + result);
     }
 
     public static int solution(String numbers) {
-        int answer = 0;
-        HashSet<String> hs = new HashSet<>();
-        for (int i = 0;i<numbers.length();i++) {
-            String num = numbers.charAt(i) + "";
-            System.out.print("num :"+num);
-            hs.put(num);
-            String value = "";
-            for (int j=numbers.length()-1;j>0;j--) {
-                if (i != j) {
-                    value += numbers.charAt(i) + "";
-                }
-            }
+        String temp = "";
+        for (int i = 1;i <=numbers.length();i++) {
+            combination(numbers, temp, i);
         }
-        
-        /*for (int i = 0;i<hs.size();i++) {
-            if(valid(Integer.parseInt(hs.get(i)))) {
-                answer++;
-            }
-        }*/
+        for (int n:arr) valid(n);
         return answer;
     }
     
-    public boolean valid(int num) {
-        int cnt = 0;
-        for (int i =2;i<num;i++) {
-            if (num%i) {
-                cnt++;
+    public static void valid(int num) {
+        if (num == 0 || num == 1) return;
+
+        for (int i =2;i<Math.sqrt(num);i++) {
+            if (num%i == 0) return;
+        }
+        answer++;
+    }
+
+    public static void combination(String n,String temp, int len) {
+        if (temp.length() == len) {
+            if (!arr.contains(Integer.parseInt(temp))) {
+                arr.add(Integer.parseInt(temp));
             }
         }
-        if (cnt == 1) {
-            return false;
-        } else {
-            return true;
+        for (int i = 0; i <n.length();i++) {
+            if (check[i]) continue;
+            temp += n.charAt(i);
+            check[i] = true;
+            combination(n,temp,len);
+            check[i] = false;
+            temp = temp.substring(0, temp.length()-1);
         }
     }
 }
