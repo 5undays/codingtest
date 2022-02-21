248a198c380ca7c27af44f396867cc5162be0ac6
SunYoung <baby_ash@naver.com>
2022-02-21 12:57:05 +0900

    다른 방법으로 풀기

--- boj/BOJ_2512.java
+++ boj/BOJ_2512.java
@@ -1,6 +1,7 @@
 package boj;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * 해당 문제는 https://www.acmicpc.net/problem/2512 문제이지만
@@ -16,7 +17,6 @@ public class BOJ_2512 {
 
     public int solution(int[] budgets, int M) {
         int answer = 0;
-        
         int left = 0;
         int right = budgets[budgets.length - 1];
 
@@ -28,10 +28,10 @@ public class BOJ_2512 {
                 right = answer - 1;
             }
         }
-        return right;
+        return answer;
     }
 
-    // 정해진 예산에 맞는지 확인 
+    // 정해진 예산에 맞는지 확인
     private boolean condition(int[] budgets, int answer, int M) {
         int totalBudget = 0;
         for (int i = 0; i < budgets.length; i++) {
@@ -46,6 +46,40 @@ public class BOJ_2512 {
             return false;
         }
         return true;
+    }
+
+    // 다른 풀이
+    private int solution1(int[] budgets, int M) {
+        int answer = 0;
+        int min = 0;
+        //int max = 0;
+        // for (int budget : budgets) {
+        //     if (budget > max) {
+        //         max = budget;
+        //     }
+        // }
+        int max = IntStream.of(budgets).max().orElse(0);
+
+        while (min <= max) {
+            final int mid = (min + max) / 2;
+            // int sum = 0;
+            // for (int budget : budgets) {
+            //     if (budget > max) {
+            //         sum += mid;
+            //     } else {
+            //         sum += budget;
+            //     }
+            // }
+            int sum = IntStream.of(budgets).map(b-> Math.min(b, mid)).sum();
+
+            if (sum <= M) {
+                min = mid + 1;
+                answer = mid;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return answer;
     }
 
 }
