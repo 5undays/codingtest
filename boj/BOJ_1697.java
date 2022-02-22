d9abeb3eb65c98cc73034b39df54fe0f93fe16fa
SunYoung <baby_ash@naver.com>
2022-02-22 13:53:00 +0900

    BFS 문제 풀기

---
+++ boj/BOJ_1697.java
@@ -0,0 +1,55 @@
+package boj;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/1697
+ */
+public class BOJ_1697 {
+
+    static final int MAX = 1000000;
+    static final boolean[] check = new boolean[MAX];
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt(); // 형이 있는 위치
+        //int k = sc.nextInt(); // 동생이 있는 위치
+        int n = 5;
+        int k = 17;
+        int answer = 0;
+        int[] dist = new int[MAX];
+        Queue<Integer> queue = new LinkedList<>();
+        // 시작점을 수동으로 넣어준다
+        check[n] = true;
+        queue.add(n);
+        while (!queue.isEmpty() || check[k] == true) { //  || visited[k] == true
+            int now = queue.remove();
+            // 접근 할 수 있는 모든 경우의 수에 해당하는 곳에 접근한다
+            if (condition(now + 1)) {
+                queue.add(now + 1);
+                check[now + 1] = true;
+                if (now + 1 == k)
+                    System.out.println("check[now + 1] :" + (now + 1));
+                dist[now + 1] = dist[now] + 1;
+            } else if (condition(now - 1)){
+                queue.add(now - 1);
+                check[now - 1] = true;
+                dist[now - 1] = dist[now] + 1;
+            } else if (condition(now * 2)) {
+                queue.add(now * 2);
+                check[now * 2] = true;
+                dist[now * 2] = dist[now] + 1;
+            } 
+        }
+        System.out.println(dist[k]);
+    }
+
+    private static boolean condition(int n) {
+        if ((n < MAX || n >= 0) || check[n]) {
+            return true;
+        }
+        return false;
+    }
+}
