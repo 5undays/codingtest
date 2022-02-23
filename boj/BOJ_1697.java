de97a9daa2f569dc2959d46ea5f93bb3d41247e2
SunYoung <baby_ash@naver.com>
2022-02-23 21:33:41 +0900

    코드 개선

--- boj/BOJ_1697.java
+++ boj/BOJ_1697.java
@@ -11,6 +11,8 @@ public class BOJ_1697 {
 
     static final int MAX = 1000000;
     static final boolean[] check = new boolean[MAX];
+    static final int[] dist = new int[MAX];
+    static final Queue<Integer> queue = new LinkedList<>();
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -18,37 +20,38 @@ public class BOJ_1697 {
         // int k = sc.nextInt(); // 동생이 있는 위치
         int n = 5;
         int k = 17;
-        int[] dist = new int[MAX];
-        Queue<Integer> queue = new LinkedList<>();
         // 시작점을 수동으로 넣어준다
         check[n] = true;
         queue.add(n);
         while (!queue.isEmpty()) {
             int now = queue.remove();
             // 접근 할 수 있는 모든 경우의 수에 해당하는 곳에 접근한다
-            if (condition(now - 1)) {
-                queue.add(now - 1);
-                check[now - 1] = true;
-                dist[now - 1] = dist[now] + 1;
-            }
-            if (condition(now + 1)) {
-                queue.add(now + 1);
-                check[now + 1] = true;
-                dist[now + 1] = dist[now] + 1;
-            }
-            if (condition(now * 2)) {
-                queue.add(now * 2);
-                check[now * 2] = true;
-                dist[now * 2] = dist[now] + 1;
-            }
+            condition(now - 1, now);
+            condition(now + 1, now);
+            condition(now * 2, now);
+            // if (condition(now - 1)) {
+                //     queue.add(now - 1);
+                //     check[now - 1] = true;
+                //     dist[now - 1] = dist[now] + 1;
+                // }
+            // if (condition(now + 1)) {
+                //     queue.add(now + 1);
+            //     check[now + 1] = true;
+            //     dist[now + 1] = dist[now] + 1;
+            // }
+            // if (condition(now * 2)) {
+            //     queue.add(now * 2);
+            //     check[now * 2] = true;
+            // }
         }
         System.out.println(dist[k]);
     }
 
-    private static boolean condition(int n) {
+    private static void condition(int n, int now) {
         if (n < MAX && n >= 0 && !check[n]) {
-            return true;
+            queue.add(n);
+            check[n] = true;
+            dist[n] = dist[now] + 1;
         }
-        return false;
     }
 }
