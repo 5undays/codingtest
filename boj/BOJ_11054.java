d1b824ccf7b3519f2eb9efddfb2c4945353f0d44
SunYoung <baby_ash@naver.com>
2022-02-28 21:46:13 +0900

    배열 정리

--- boj/BOJ_11054.java
+++ boj/BOJ_11054.java
@@ -18,17 +18,17 @@ public class BOJ_11054 {
 
         // 올림 차순 배열 길이
         for (int i = 0; i < arr.length; i++) {
-            for (int j = arr.length - 1; j > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (arr[i] > arr[j])
                     leftArr[i] = Math.max(leftArr[i], leftArr[j] + 1);
             }
         }
 
-        // 내림 차순
-        for (int i = arr.length; i > 0; i--) {
-            for (int j = 0; j < arr.length; i++) {
+        // 내림 차순 배열 길이
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = arr.length - 1; j > i; j--) {
                 if (arr[i] > arr[j])
-                    rightArr[i] = Math.max(rightArr[i], rightArr[j]);
+                    rightArr[i] = Math.max(rightArr[i], rightArr[j] + 1);
             }
         }
 
