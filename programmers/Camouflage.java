31dcdd6a40583c16d144589c7929c3d9a8b0a3fe
SunYoung <baby_ash@naver.com>
2022-03-08 13:35:54 +0900

    위장 다른 풀이 추가

--- programmers/Camouflage.java
+++ programmers/Camouflage.java
@@ -1,7 +1,7 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class Camouflage {
 
@@ -9,7 +9,7 @@ public class Camouflage {
         Camouflage camouflage = new Camouflage();
         int answer1 = camouflage.solution(new String[][] { { "yellowhat", "headgear" }, { "bluesunglasses", "eyewear" },
                 { "green_turban", "headgear" } });
-        int answer2 = camouflage.solution(
+        int answer2 = camouflage.solution2(
                 new String[][] { { "crowmask", "face" }, { "bluesunglasses", "face" }, { "smoky_makeup", "face" } });
         System.out.println(answer1);
         System.out.println(answer2);
@@ -27,6 +27,15 @@ public class Camouflage {
             answer *= methods.get(method) + 1;
         }
         return answer - 1;
+    }
+
+    private int solution2(String[][] clothes) {
+        return Arrays.stream(clothes)
+                .map(type -> clothes[1])
+                .distinct()
+                .map(type -> (int) Arrays.stream(clothes).filter(cloth -> clothes[1].equals(type)).count())
+                .map(type -> type + 1)
+                .reduce(1, (c, n) -> c * n) - 1;
     }
 
 }
