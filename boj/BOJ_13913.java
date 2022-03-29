d04573472fa307d14257ffa3053e943932941fe1
SunYoung <baby_ash@naver.com>
2022-03-29 18:43:19 +0900

    숏코드

--- boj/BOJ_13913.java
+++ boj/BOJ_13913.java
@@ -4,46 +4,30 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
-import java.util.concurrent.locks.Condition;
 
 // https://www.acmicpc.net/problem/13913
 public class BOJ_13913 {
 
     static final int MAX = 1000000;
     static final boolean[] check = new boolean[MAX]; // 방문 여부
+    static Queue<Integer> queue = new LinkedList<>(); // 방문 큐
+    static int[] dist = new int[MAX];
+    static int[] prevDist = new int[MAX];
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        int n = sc.nextInt(); // 형이 있는 위치
+        int k = sc.nextInt(); // 동생이 있는 위치
 
-        Queue<Integer> queue = new LinkedList<>(); // 방문 큐
-        int[] dist = new int[MAX];
-        int[] prevDist = new int[MAX];
         queue.add(n);
         while (!queue.isEmpty()) {
             int now = queue.remove();
-            if (condition(now - 1)) {
-                queue.add(now - 1);
-                check[now - 1] = true;
-                dist[now - 1] = dist[now] + 1;
-                prevDist[now - 1] = now;
-            }
-            if (condition(now + 1)) {
-                queue.add(now + 1);
-                check[now + 1] = true;
-                dist[now + 1] = dist[now] + 1;
-                prevDist[now + 1] = now;
-            }
-            if (condition(now * 2)) {
-                queue.add(now * 2);
-                check[now * 2] = true;
-                dist[now * 2] = dist[now] + 1;
-                prevDist[now * 2] = now;
-            }
+            condition(now - 1, now);
+            condition(now + 1, now);
+            condition(now * 2, now);
         }
 
-        System.out.println(dist[k]);
+        System.out.println(dist[k]); // 동생을 찾는 가장 빠른 시간
         Stack<Integer> result = new Stack<>();
         result.push(k);
         int idx = k;
@@ -53,13 +37,16 @@ public class BOJ_13913 {
         }
 
         while (!result.isEmpty()) {
-            System.out.print(result.pop() + " ");
+            System.out.print(result.pop() + " "); // 가장 빨리 찾는 이동 방법
         }
     }
 
-    private static boolean condition(int num) {
+    private static boolean condition(int num, int now) {
         if (num >= 0 && num < MAX && !check[num]) {
-            return true;
+            queue.add(num);
+            check[num] = true;
+            dist[num] = dist[now] + 1;
+            prevDist[num] = now;
         }
         return false;
     }
