c12e24b35f3dc687d33d488b4fd8ddb2397ae277
SunYoung <baby_ash@naver.com>
2022-04-02 21:28:44 +0900

    테스트 추가

--- interview/MathExpression.java
+++ interview/MathExpression.java
@@ -6,18 +6,22 @@ import java.util.Stack;
 
 public class MathExpression {
 
+    List<Character> openingBrackets = Arrays.asList('(', '{', '[');
+    List<Character> closingBrackets = Arrays.asList(')', '}', ']');
+
     public static void main(String[] args) {
         MathExpression mathExpression = new MathExpression();
         boolean chk = mathExpression.check("[{(mathExpression)}");
         System.out.println(chk);
+        
+        int idx = mathExpression.matchIndex("[{1+2*(2+2)}-(1-3)]", 0);
+        System.out.println(idx);
     }
 
     // 괄호 짝 체크 문제
     private boolean check(String mathExpression) {
         Stack<Character> brackets = new Stack<>();
         char[] chars = mathExpression.toCharArray();
-        List<Character> openingBrackets = Arrays.asList('(', '{', '[');
-        List<Character> closingBrackets = Arrays.asList(')', '}', ']');
 
         for (char c : chars) {
             if (openingBrackets.contains(c)) {
@@ -35,5 +39,37 @@ public class MathExpression {
         }
 
         return brackets.isEmpty();
+    }
+
+    class NodeTest {
+        Character bracket;
+        int index;  
+    }
+    /**
+     * matchIdx 에 대응하는 괄호 위치 찾기
+     * 
+     * @param expression 괄호 수식
+     * @param matchIdx   여는 괄호
+     * @return 닫는 괄호 위치
+     */
+    private int matchIndex(String expression, int matchIdx) {
+        Stack<NodeTest> brackets = new Stack<>();
+        char close = closingBrackets.get(matchIdx);
+        char[] exp = expression.toCharArray();
+        
+        for (int i = 0; i < exp.length; i++) {
+            if (openingBrackets.contains(exp[i])) {
+                NodeTest nodeTest = new NodeTest();
+                nodeTest.index = i;
+                nodeTest.bracket = exp[i];
+                brackets.push(nodeTest);
+            } else if (closingBrackets.contains(exp[i])) {
+                NodeTest openingBracket = brackets.pop();
+                if (closingBrackets.indexOf(exp[i]) != openingBrackets.indexOf(openingBracket)) {
+                    return openingBracket.index;
+                }
+            }
+        }
+        return 0;
     }
 }
