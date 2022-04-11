99f6db52f8f33df115932213fe10dba004e6e86c
SunYoung <baby_ash@naver.com>
2022-04-11 16:48:21 +0900

    오타

--- boj/BOJ_9095.java
+++ boj/BOJ_9095.java
@@ -1,5 +1,7 @@
 package boj;
 
+import java.util.Scanner;
+
 public class BOJ_9095 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,16 +12,18 @@ public class BOJ_9095 {
         }
 
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(go(0,numbers[i]));
+            System.out.println(go(0, numbers[i]));
         }
     }
 
-    public int go(int sum, int goal) {
-        if (sum > goal) return 0;
-        if (sum == goal) return 1;
+    public static int go(int sum, int goal) {
+        if (sum > goal)
+            return 0;
+        if (sum == goal)
+            return 1;
         int now = 0;
         for (int i = 1; i <= 3; i++) {
-            now += go(sum + i, goal)
+            now += go(sum + i, goal);
         }
         return now;
     }
