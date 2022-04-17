d265c72cfc315218824b2ceaa404e3143b0316ba
SunYoung <baby_ash@naver.com>
2022-04-17 21:24:45 +0900

    테트로미노

--- boj/BOJ_14500.java
+++ boj/BOJ_14500.java
@@ -8,43 +8,89 @@ public class BOJ_14500 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); // 가로
-        int M  = sc.nextInt(); // 세로
+        int M = sc.nextInt(); // 세로
         int[][] numbers = new int[N][M];
         for (int i = 0; i < N; i++) {
-            for (int j = i; i <M; j++) {
+            for (int j = i; i < M; j++) {
                 numbers[i][j] = sc.nextInt();
             }
         }
 
         int answer = 0;
 
-        for (int i = 0; i < N;i ++) {
-            for (int j = 0 ; j < M; j++) {
-                if (i+3 < N ) {
-                    int temp = numbers[i][j] + numbers[i][j+1] + numbers[i][j+2]+ numbers[i][j+3];
-                    if (answer < temp) answer = temp;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (j + 3 < M) {
+                    int temp = numbers[i][j] + numbers[i][j + 1] + numbers[i][j + 2] + numbers[i][j + 3];
+                    if (answer < temp)
+                        answer = temp;
                 }
-                if (j+3 < M) {
-                    int temp = numbers[i][j] + numbers[i+1][j] + numbers[i+2][j] + numbers[i+3][j];
-                    if (answer < temp) answer = temp;
+                if (i + 3 < N) {
+                    int temp = numbers[i][j] + numbers[i + 1][j] + numbers[i + 2][j] + numbers[i + 3][j];
+                    if (answer < temp)
+                        answer = temp;
+                }
+                if (i + 1 < N && j + 2 < M) {
+                    int temp = numbers[i][j] + numbers[i + 1][j] + numbers[i + 1][j + 1] + numbers[i + 1][j + 2];
+                    if (answer < temp)
+                        answer = temp;
+                }
+                if (i + 2 < N && j + 1 < M) {
+                    int temp = numbers[i][j] + numbers[i][j + 1] + numbers[i + 1][j] + numbers[i + 2][j];
+                    if (answer < temp)
+                        answer = temp;
+                }
+                if (i + 1 < N && j + 1 < M) {
+                    int temp = numbers[i][j] + numbers[i + 1][j] + numbers[i][j + 1] + numbers[i + 1][1 + i];
+                    if (answer < temp)
+                        answer = temp;
+                }
+                if (i - 1 >= 0 && j + 2 < M) {
+                    int temp = numbers[i][j] + numbers[i][j + 1] + numbers[i - 1][j + 1] + numbers[i - 1][j + 2];
+                    if (answer < temp)
+                        answer = temp;
+                }
+                if (i + 2 < N && j + 1 < M) {
+                    int temp = numbers[i][j] + numbers[i + 1][j] + numbers[i + 1][j + 1] + numbers[i + 2][j + 1];
+                    if (answer < temp)
+                        answer = temp;
+                }
+                if (i + 1 < N && j + 2 < M) {
+                    int temp = numbers[i][j] + numbers[i][j + 1] + numbers[i + 1][j + 1] + numbers[i + 1][j + 2];
+                    if (answer < temp)
+                        answer = temp;
                 }
 
-                if (i+1 < N && j+1 < M) {
-                    int temp = numbers[i][j] + numbers[i+1][j] + numbers[i][j+1] + numbers[i+1][1+i];
-                    if (answer < temp) answer = temp;
+                if (j + 2 < M) {
+                    int temp = numbers[i][j] + numbers[i][j + 1] + numbers[i][j + 2];
+                    if (i - 1 >= 0) {
+                        int temp2 = temp + numbers[i - 1][j + 1];
+                        if (answer < temp2)
+                            answer = temp2;
+                    }
+                    if (i + 1 < N) {
+                        int temp2 = temp + numbers[i + 1][j + 1];
+                        if (answer < temp2)
+                            answer = temp2;
+                    }
                 }
 
-                if (i+1 < N && j+2 < M) {
-                    int temp = numbers[i][j] + numbers[i+1][j] + numbers[i+1][j+1] + numbers[i+1][j+2];
-                    if (answer < temp) answer = temp;
-                }
+                if (i + 2 < N) {
+                    int temp = numbers[i][j] + numbers[i + 1][j] + numbers[i + 2][j];
+                    if (j + 1 < M) {
+                        int temp2 = temp + numbers[i + 1][j + 1];
+                        if (answer < temp2)
+                            answer = temp2;
+                    }
+                    if (j - 1 >= 0) {
+                        int temp2 = temp + numbers[i + 1][j - 1];
+                        if (answer < temp2)
+                            answer = temp2;
+                    }
 
-                if (i+2 < N && j+1 < M) {
-                    int temp = numbers[i][j] + numbers[i][j+1];
                 }
-
             }
         }
-        System.out.println();       
+        System.out.println(answer);
     }
 }
