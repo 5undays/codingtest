ca0be98db49e181da81d8770c32a23db1862d8b9
SunYoung <baby_ash@naver.com>
2022-05-24 20:23:24 +0900

    최소공배수

---
+++ boj/BOJ_1934.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int g = gcd(a, b);
+            System.out.println(a * b / g);
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
