63473c4479913e5197c54a48dc6ec0c26c360cb0
SunYoung <baby_ash@naver.com>
2022-05-24 19:10:12 +0900

    dd

---
+++ boj/BOJ_1978.java
@@ -0,0 +1,11 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_1978 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+    }
+}
