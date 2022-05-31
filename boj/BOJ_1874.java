06d1c2591ee15c2c66140a21223137895d5d7cd2
SunYoung <baby_ash@naver.com>
2022-05-31 14:40:30 +0900

    stack

---
+++ boj/BOJ_1874.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BOJ_1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        valid(arr);
+    }
+
+    public static void valid(int[] arr) {
+        Stack<Integer> s = new Stack();
+        int i = 0;
+        StringBuffer sb = new StringBuffer();
+        for (int x : arr) {
+            if (i < x) { // 배열보다 스택에 쌓일 값이 작을때
+                while (x > i) { // 스택에 쌓인 값이 쌓여질 값보다 커질때까지
+                    s.push(++i);
+                    sb.append("+\n");
+                }
+            } else { 
+                if (s.peek() != x) { // 배열보다 스택에 쌓일 값이 같지 않을때 
+                    System.out.println("NO");
+                    break;
+                }
+            }
+            s.pop(); 
+            sb.append("-\n");
+        }
+    }
+}
