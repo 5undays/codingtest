bc07e4838e560e57344e1faef069c53d7f3a4c70
SunYoung <baby_ash@naver.com>
2022-06-02 16:54:50 +0900

    BFS

---
+++ boj/BOJ_1158.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_1158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int per = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < per - 1; j++) {
+                q.offer(q.poll());
+            }
+            sb.append(q.poll() + ", ");
+        }
+        sb.append(q.poll() + ">");
+        System.out.println(sb);
+    }
+}
