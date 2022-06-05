27660af40639e2f0e8bd64f8847ca22d9dbe31d2
SunYoung <baby_ash@naver.com>
2022-06-05 15:48:59 +0900

    오큰수

--- boj/BOJ_17299.java
+++ boj/BOJ_17299.java
@@ -3,32 +3,34 @@ package boj;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class BOJ_17298 {
+// 오등큰수
+public class BOJ_17299 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] data = new int[n];
         int[] answer = new int[n];
-        Stack<Integer> stack = new Stack<>();
+        int[] data = new int[n];
+        int[] frequence = new int[1000001];
         for (int i = 0; i < n; i++) {
             data[i] = sc.nextInt();
+            frequence[data[i]]++;
         }
 
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
             if (stack.isEmpty()) {
                 stack.push(i);
             }
-            while (!stack.isEmpty() && answer[stack.peek()] < answer[i]) {
-                answer[stack.pop()] = answer[i];
+            while (!stack.isEmpty() && frequence[data[stack.peek()]] < frequence[data[i]]) {
+                answer[stack.pop()] = data[i];
             }
             stack.push(i);
         }
 
-        // 모든 값을 다 대입했을때도 비교군이 남아있을 경우 해당 값은 가장 큰 값이 된다
         while (!stack.empty()) {
             answer[stack.pop()] = -1;
         }
-        
+
         for (int i = 0; i < n; i++) {
             System.out.println(answer[i]);
         }
