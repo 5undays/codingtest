3aac649a92b7314c24d1b4af17b0f44409e05fbc
SunYoung <baby_ash@naver.com>
2022-06-30 07:21:06 +0900

    수 찾기

--- boj/BOJ_1920.java
+++ boj/BOJ_1920.java
@@ -1,24 +1,44 @@
 package boj;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class BOJ_1920 {
+    static int n;
+    static ArrayList<Integer> list;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Integer> list = new ArrayList<>();
+        n = sc.nextInt();
+        list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             list.add(sc.nextInt());
         }
+        Collections.sort(list);
         int m = sc.nextInt();
         for (int i = 0; i < m; i++) {
-            int b = sc.nextInt();
-            if (list.contains(b)) {
+            if (binarySearch(sc.nextInt()) >= 0) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
             }
         }
+    }
+
+    private static int binarySearch(int x) {
+        int start = 0, end = list.size() - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int midValue = list.get(mid);
+            if (midValue > x) {
+                end = mid - 1;
+            } else if (midValue < x) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
