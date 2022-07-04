69ab31a9840844dc4fe3f6378973395af0497c09
SunYoung <baby_ash@naver.com>
2022-07-04 22:43:05 +0900

    정렬

--- boj/BOJ_11650.java
+++ boj/BOJ_11650.java
@@ -1,29 +1,52 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Scanner;
 
+/**
+ * 좌표 정렬하기
+ * https://www.acmicpc.net/problem/11650
+ */
 public class BOJ_11650 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        //Arrayli
-        for (int i= 0;i < n; i++) {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Dimension> xy = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            Dimension d = new Dimension(sc.nextInt(), sc.nextInt());
+            xy.add(d);
         }
+
+        Collections.sort(xy, new Comparator<Dimension>() {
+
+            @Override
+            public int compare(Dimension o1, Dimension o2) {
+                // TODO Auto-generated method stub
+                if (o1.x == o2.x) {
+                    return o1.y - o2.y;
+                } else {
+                    return o1.x - o2.x;
+                }
+            }
+
+        });
+        StringBuilder sb = new StringBuilder();
+        for (Dimension d : xy) {
+            sb.append(d.x).append(" ").append(d.y).append("\n");
+        }
+        System.out.println(sb);
     }
 }
 
-class Dimension implements Comparator {
+class Dimension {
     int x;
     int y;
 
-    @Override
-    public int compare(Object o1, Object o2) {
-        // TODO Auto-generated method stub
-        return 0;
+    Dimension(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
 }
