2c9b44c3c5a528b9e4ad6ce1ffb825e752e5e4f6
SunYoung <baby_ash@naver.com>
2022-07-19 16:41:17 +0900

    소수

---
+++ boj/BOJ_14889.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_14889 {
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        backtracking(0); 
+    }
+
+    private static void backtracking(int n) {
+        if ()
+    } 
+}
