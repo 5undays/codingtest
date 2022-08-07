a42659429b1b017db5db8ce528a8a5351676f06e
SunYoung <baby_ash@naver.com>
2022-08-07 15:57:04 +0900

    오타 수정

--- boj/BOJ_1874.java
+++ boj/BOJ_1874.java
@@ -3,8 +3,13 @@ package boj;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * 스택 수열
+ * https://www.acmicpc.net/problem/1874
+ */
 public class BOJ_1874 {
     static StringBuffer sb = new StringBuffer();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,27 +17,25 @@ public class BOJ_1874 {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        valid(arr);
-        System.out.println(sb);
-    }
 
-    public static void valid(int[] arr) {
-        Stack<Integer> s = new Stack();
         int i = 0;
+        
+        Stack<Integer> s = new Stack();
         for (int x : arr) {
             if (i < x) { // 배열보다 스택에 쌓일 값이 작을때
                 while (x > i) { // 스택에 쌓인 값이 쌓여질 값보다 커질때까지
                     s.push(++i);
                     sb.append("+\n");
                 }
-            } else { 
-                if (s.peek() != x) { // 배열보다 스택에 쌓일 값이 같지 않을때 
+            } else {
+                if (s.peek() != x) { // 배열보다 스택에 쌓일 값이 같지 않을때
                     System.out.println("NO");
-                    break;
+                    System.exit(0);
                 }
             }
-            s.pop(); 
+            s.pop();
             sb.append("-\n");
         }
+        System.out.println(sb);
     }
 }
