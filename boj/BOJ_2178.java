9d254952cc5d16e11fa3b850bdbeeb5c72142018
SunYoung <baby_ash@naver.com>
2022-08-09 10:26:06 +0900

    정리

--- boj/BOJ_2178.java
+++ boj/BOJ_2178.java
@@ -31,38 +31,13 @@ public class BOJ_2178 {
         }
 
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
+        q.add(new Pair(0, 0)); // 1,1 에서 출발하지만 물리적으론 0,0 으로 설정해줌
         check[0][0] = true;
         distance[0][0] = 1;
         while (!q.isEmpty()) {
             Pair p = q.remove();
             int x = p.x;
             int y = p.y;
-            for (int i = 0; i < dx.length; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (check[nx][ny] == false && map[nx][ny] == 1) {
-                        q.add(new Pair(nx, ny));
-                        distance[nx][ny] = distance[x][y] + 1;
-                        check[nx][ny] = true;
-                    }
-                }
-            }
-        }
-        System.out.println(distance[n - 1][m - 1]);
-
-    }
-
-    private static void bfs(int x, int y, int n, int m) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(x, y));
-        check[x][y] = true;
-        distance[x][y] = 1;
-        while (!q.isEmpty()) {
-            Pair p = q.remove();
-            x = p.x;
-            y = p.y;
             for (int i = 0; i < dx.length; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
