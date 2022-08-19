e0576c9aa2c65f98466c0dd81df0168b50ea1306
SunYoung <baby_ash@naver.com>
2022-08-19 23:45:40 +0900

    영화감독 숌

---
+++ boj/BOJ_1436.java
@@ -0,0 +1,53 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+ * 영화감독 숌
+ * https://www.acmicpc.net/problem/1436
+ */
+public class BOJ_1436 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 1) {
+            getMovieName(n);
+        } else {
+            System.out.println(666);
+        }
+    }
+
+    private static void getMovieName(int n) {
+        int movieName = 666;
+        int cycle = 0, temp = 0, digit = 0;
+        int idx = 1;
+        while (true) {
+            movieName++;
+            digit = movieName;
+            // 666 연속 검증    
+            while (digit > 0) {
+                temp = digit % 10;
+                if (temp == 6) {
+                    cycle++;
+                } else if (cycle < 3) {
+                    cycle = 0;
+                }
+                digit = digit / 10;
+            }
+
+            // 3자리 연속을 넘으면 영화이름의 IDX개수가 증가
+            if (cycle >= 3) {
+                idx++;
+            }
+
+            cycle = 0;
+
+            // IDX 번째 영화 제목
+            if (idx == n) {
+                System.out.println(movieName);
+                break;
+            }
+        }
+
+    }
+}
