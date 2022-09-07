3f8fde8494918932c3a92125c513f0c0fa6d5dfd
SunYoung <baby_ash@naver.com>
2022-09-08 03:29:05 +0900

    벌집

---
+++ boj/BOJ_2292.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+ * 벌집
+ * https://www.acmicpc.net/problem/2292
+ */
+public class BOJ_2292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        int a = 1;
+        int answer = 1;
+        if (n != 1) {
+            while (a < n) {
+                a = a + (6 * answer);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
