9c67a96c045901a51296608ac4c1e604ef590964
SunYoung <baby_ash@naver.com>
2022-09-09 20:48:25 +0900

    수 정렬하기 3

--- boj/BOJ_10989.java
+++ boj/BOJ_10989.java
@@ -3,45 +3,28 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 /**
  * 수 정렬하기 3
  * https://www.acmicpc.net/problem/10989
  */
 public class BOJ_10989 {
-    static ArrayList<Integer> list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        list = new ArrayList<>();
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        // Collections.sort(list);
-        countingSort();
 
-        for (int x : list) {
-            sb.append(x).append("\n");
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static void countingSort() {
-        int[] arr = new int[10001];
-        for (int x : list) {
-            arr[x]++;
-        }
-
-        int reset = 0;
-        for (int i = 0; i < arr.length; i++) {
-            while (arr[i] > 0) {
-                list.add(reset++, i);
-                arr[i]--;
-            }
-        }
     }
 }
