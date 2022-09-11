b8e2d304692a32a97d26a40534d610452295b3be
SunYoung <baby_ash@naver.com>
2022-09-11 20:41:10 +0900

    아스키코드

---
+++ boj/BOJ_11654.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.io.IOException;
+
+/**
+ * 아스키코드
+ * https://www.acmicpc.net/problem/11654
+ */
+public class BOJ_11654 {
+    public static void main(String[] args) throws IOException {
+        int answer = System.in.read();
+        System.out.println(answer);
+    }
+}
