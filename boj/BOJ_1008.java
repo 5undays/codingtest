0248071ad8fbb0a9aa63e7eebc758be1ed1209c9
SunYoung <baby_ash@naver.com>
2022-09-13 22:19:36 +0900

    이항계수1

--- boj/BOJ_1008.java
+++ boj/BOJ_1008.java
@@ -1,24 +1,17 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 /**
  * A/B
  * https://www.acmicpc.net/problem/1008
  */
 public class BOJ_1008 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
- 		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str," ");
-		double a = Double.parseDouble(st.nextToken());
-		double b = Double.parseDouble(st.nextToken());
-		
-		System.out.println(a/b);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        sc.close();
+        System.out.println( a / b);
     }
 }
