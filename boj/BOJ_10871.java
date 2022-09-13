8dbc8a7c91d7cfe2f5b364f6d24ad277e021bff1
SunYoung <baby_ash@naver.com>
2022-09-13 22:50:21 +0900

    X보다 작은 수

---
+++ boj/BOJ_10871.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+ * X보다 작은 수
+ * https://www.acmicpc.net/problem/10871
+ */
+public class BOJ_10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            if (m < x) {
+                sb.append(m).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
