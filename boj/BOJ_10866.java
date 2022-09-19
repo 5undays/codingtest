e7096763a418ce26614a14827e62f6dc0071bb9f
SunYoung <baby_ash@naver.com>
2022-09-19 15:42:43 +0900

    덱

--- boj/BOJ_10866.java
+++ boj/BOJ_10866.java
@@ -1,16 +1,23 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Scanner;
 
+/**
+ * 덱
+ * https://www.acmicpc.net/problem/10866
+ */
 public class BOJ_10866 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         Deque<Integer> d = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            String[] line = sc.next().split(" ");
+            String[] line = br.readLine().split(" ");
             String cmd = line[0].toString();
             if (cmd.equals("push_front")) {
                 int data = Integer.parseInt(line[1]);
@@ -20,31 +27,38 @@ public class BOJ_10866 {
                 d.addLast(data);
             } else if (cmd.equals("pop_front")) {
                 if (d.isEmpty()) {
-                    System.out.println(-1);
+                    sb.append(-1).append("\n");
                 } else {
-                    System.out.println(d.pollFirst());
+                    sb.append(d.pollFirst()).append("\n");
+                }
+            } else if (cmd.equals("pop_back")) {
+                if (d.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(d.pollLast()).append("\n");
                 }
             } else if (cmd.equals("size")) {
-                System.out.println(d.size());
+                sb.append(d.size()).append("\n");
             } else if (cmd.equals("empty")) {
                 if (d.isEmpty()) {
-                    System.out.println(1);
+                    sb.append(1).append("\n");
                 } else {
-                    System.out.println(0);
+                    sb.append(0).append("\n");
                 }
             } else if (cmd.equals("front")) {
                 if (d.isEmpty()) {
-                    System.out.println(-1);
+                    sb.append(-1).append("\n");
                 } else {
-                    System.out.println(d.peekFirst());
+                    sb.append(d.peekFirst()).append("\n");
                 }
             } else if (cmd.equals("back")) {
                 if (d.isEmpty()) {
-                    System.out.println(-1);
+                    sb.append(-1).append("\n");
                 } else {
-                    System.out.println(d.peekLast());
+                    sb.append(d.peekLast()).append("\n");
                 }
             }
         }
+        System.out.println(sb);
     }
 }
