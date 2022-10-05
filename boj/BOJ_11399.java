a38f39821ed996fb15a22b0d97417c199b5a37b4
SunYoung <baby_ash@naver.com>
2022-10-05 22:07:01 +0900

    ATM

---
+++ boj/BOJ_11399.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * ATM
+ * https://www.acmicpc.net/problem/11399
+ */
+public class BOJ_11399 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Arrays.sort(arr);
+        int sum = 0;
+        int prev = 0;
+        for (int i = 0; i < n; i++) {
+            sum += prev + arr[i];
+            prev += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
