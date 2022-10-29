bd82f1fc4f4e63e970b10b4b151456a1970805d5
SunYoung <baby_ash@naver.com>
2022-10-29 14:37:23 +0900

    좌표 정렬하기 2

--- boj/BOJ_11651.java
+++ boj/BOJ_11651.java
@@ -13,13 +13,13 @@ public class BOJ_11651 {
         int n = sc.nextInt();
         int[][] xy = new int[n][2];
         for (int i = 0; i < n; i++) {
-            xy[i][0] = sc.nextInt();
-            xy[i][1] = sc.nextInt();
+            xy[i][0] = sc.nextInt(); // x
+            xy[i][1] = sc.nextInt(); // y
         }
 
         Arrays.sort(xy, (o1, o2) -> {
-            if (o1[1] == o2[1]) {
-                return o1[0] - o1[0];
+            if (o1[1] == o2[1]) { // y 가 같으면
+                return o1[0] - o2[0];
             } else {
                 return o1[1] - o2[1];
             }
