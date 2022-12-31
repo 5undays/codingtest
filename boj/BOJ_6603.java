d8547ec5f16c79354b9a78b5992a1d1a084b5a79
SunYoung <baby_ash@naver.com>
2022-12-31 21:59:48 +0900

    로또

--- boj/BOJ_6603.java
+++ boj/BOJ_6603.java
@@ -20,27 +20,29 @@ public class BOJ_6603 {
             }
             boolean[] check = new boolean[k];
             backtracking(0, 0, s, check);
-
+            System.out.println();
         }
     }
 
     /**
      * idx 부터 중복 없는 오름차순으로 뽑는다
-     * @param idx 현재 인덱스
+     *
+     * @param idx   현재 인덱스
      * @param depth 중복 없이 뽑는 갯수
-     * @param s 전체 배열
-     * @param c 중복 없는 배열 방문 여부
+     * @param s     전체 배열
+     * @param c     중복 없는 배열 방문 여부
      */
     private static void backtracking(int idx, int depth, int[] s, boolean[] c) {
         if (depth == 6) {
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < s.length; i++) {
                 if (c[i]) {
-                    System.out.print(s[i] + " ");
+                    sb.append(s[i] + " ");
                 }
             }
-            System.out.println();
+            System.out.println(sb);
+            return;
         }
-
         for (int i = idx; i < s.length; i++) {
             c[i] = true;
             backtracking(i + 1, depth + 1, s, c);
