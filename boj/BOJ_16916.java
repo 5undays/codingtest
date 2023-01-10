2effde37efd260c9559137db06db5a7e0f0a06bb
SunYoung <baby_ash@naver.com>
2023-01-10 20:33:08 +0900

    부분 문자열

---
+++ boj/BOJ_16916.java
@@ -0,0 +1,43 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 부분 문자열
+ * https://www.acmicpc.net/problem/16916
+ */
+public class BOJ_16916 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String p = br.readLine();
+        int j = 0;
+        int[] pi = new int[p.length()];
+        for (int i = 1; i < p.length(); i++) {
+            while (j > 0 && p.charAt(i) != p.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (p.charAt(i) == p.charAt(j)) {
+                pi[i] = j += 1;
+            }
+        }
+
+        j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            while (j > 0 && s.charAt(i) != p.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (s.charAt(i) == p.charAt(j)) {
+                if (j == p.length() - 1) {
+                    System.out.println(1);
+                    System.exit(0);
+                } else {
+                    j++;
+                }
+            }
+        }
+        System.out.println(0);
+    }
+}
