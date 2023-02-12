bd79eaf714221853e0285ac54826c963c7fd70ce
SunYoung <baby_ash@naver.com>
2023-02-12 21:27:14 +0900

    가까운 수

---
+++ programmers/PGS_120890.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.Arrays;
+
+/**
+ * 가까운 수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120890
+ */
+public class PGS_120890 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{3, 10, 28}, 20)); // 28
+        System.out.println(solution(new int[]{10, 11, 12}, 13)); // 12
+        System.out.println(solution(new int[]{1, 3, 5, 7, 9, 11, 13, 15}, 2)); // 1
+        System.out.println(solution(new int[]{15, 11, 13}, 14)); // 13
+        System.out.println(solution(new int[]{9, 11, 12}, 10)); // 9
+        System.out.println(solution(new int[]{9, 11, 12}, 11)); // 9
+    }
+
+    public static int solution(int[] array, int n) {
+        int answer = 0;
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            answer = Math.abs(array[i] - n) >= Math.abs(array[answer] - n) ? answer : i;
+        }
+        return array[answer];
+    }
+}
