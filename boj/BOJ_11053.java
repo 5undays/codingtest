182378541e6a3680a9198fbffd24b6cf0d3a335d
SunYoung <baby_ash@naver.com>
2023-02-25 15:09:52 +0900

    가장 긴 증가하는 부분 수열

---
+++ boj/BOJ_11053.java
@@ -0,0 +1,34 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 가장 긴 증가하는 부분 수열
+ * https://www.acmicpc.net/problem/11053
+ */
+public class BOJ_11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            answer = Math.max(dp[i], answer);
+        }
+        System.out.println(answer);
+    }
+}
