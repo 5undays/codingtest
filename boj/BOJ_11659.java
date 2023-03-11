ee64f771ada41dfef151ee6a17e2f3a9edaacf07
SunYoung <baby_ash@naver.com>
2023-03-11 13:41:32 +0900

    구간 합 구하기 4

---
+++ boj/BOJ_11659.java
@@ -0,0 +1,44 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 구간 합 구하기 4
+ * https://www.acmicpc.net/problem/11659
+ */
+public class BOJ_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + arr[i];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < m; k++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            if (i - 2 >= 0) {
+                sb.append(dp[j - 1] - dp[i - 2]).append("\n");
+            } else {
+                sb.append(dp[j - 1]).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
