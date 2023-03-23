060a3525abf8a4bb3b550bb125782bd7d316af45
SunYoung <baby_ash@naver.com>
2023-03-23 13:04:44 +0900

    크로아티아 알파벳

---
+++ boj/BOJ_2941.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+ * 크로아티아 알파벳
+ * https://www.acmicpc.net/problem/2941
+ */
+public class BOJ_2941 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int answer = s.length();
+
+        for (int i = 1; i < s.length(); i++) {
+            char c = s.charAt(i);
+            char before = s.charAt(i - 1);
+            if (c == '=') {
+                if (before == 'c') answer--;
+                if (before == 's') answer--;
+                if (before == 'z') {
+                    answer--;
+                    if (i - 2 >= 0) {
+                        if (s.charAt(i - 2) == 'd') answer--;
+                    }
+                }
+            }
+            if (c == '-') {
+                if (before == 'c') answer--;
+                if (before == 'd') answer--;
+            }
+            if (c == 'j') {
+                if (before == 'l') answer--;
+                if (before == 'n') answer--;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
