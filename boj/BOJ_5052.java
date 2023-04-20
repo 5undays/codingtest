c4db277e065b216edb98b914883e49f06e30a684
SunYoung <baby_ash@naver.com>
2023-04-20 17:58:42 +0900

    전화번호 목록

---
+++ boj/BOJ_5052.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 전화번호 목록
+ * https://www.acmicpc.net/problem/5052
+ */
+public class BOJ_5052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String[] arr = new String[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = br.readLine();
+            }
+            Arrays.sort(arr);
+            String result = "YES";
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i + 1].length() < arr[i].length()) {
+                    continue;
+                } else if (arr[i + 1].startsWith(arr[i])) {
+                    result = "NO";
+                    break;
+                }
+            }
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
