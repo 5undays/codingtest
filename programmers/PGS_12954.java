4e0c4b83885ae2e416e1a348e2a6df504a05246e
SunYoung <baby_ash@naver.com>
2023-05-06 23:48:13 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ programmers/PGS_12954.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.Arrays;
+
+/**
+ * x만큼 간격이 있는 n개의 숫자
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12954
+ */
+public class PGS_12954 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(2, 5)));
+        System.out.println(Arrays.toString(solution(4, 3)));
+        System.out.println(Arrays.toString(solution(-4, 2)));
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        int index = 0;
+        int value = x;
+        while (index < n) {
+            answer[index++] = value;
+            value += x;
+        }
+        return answer;
+    }
+}
