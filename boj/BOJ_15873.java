4a324c2d8df398ebad8b48af33501fe9bb1f6a9d
SunYoung <baby_ash@naver.com>
2023-05-29 01:24:14 +0900

    공백 없는 A+B

--- boj/BOJ_15873.java
+++ boj/BOJ_15873.java
@@ -10,14 +10,11 @@ public class BOJ_15873 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int a = 0;
-        if (n == 1010) {
-            System.out.println(20);
-        } else if (n % 10 == 0) {
+        if (n % 10 == 0) {
             n /= 100;
             System.out.println(10 + n);
         } else {
-            a += n % 10;
+            int a = n % 10;
             n /= 10;
             if (n == 10) {
                 System.out.println(10 + a);
