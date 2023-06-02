b46d26229139e8fc922c9d3600bf37fb7b5bd84b
SunYoung <baby_ash@naver.com>
2023-06-02 20:28:08 +0900

    의상

---
+++ programmers/PGS_42578.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * 의상
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42578
+ */
+public class PGS_42578 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            String type = clothes[i][1];
+            if (!hm.containsKey(type)) {
+                hm.put(type, 1);
+            } else {
+                hm.put(type, hm.get(type) + 1);
+            }
+        }
+
+        Iterator<Integer> iter = hm.values().iterator();
+
+        while (iter.hasNext()) {
+            answer *= iter.next().intValue() + 1;
+        }
+
+        return answer - 1;
+    }
+}
