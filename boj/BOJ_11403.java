919b5537d2f7e6e39a586ee7e65678b8852d9f7d
SunYoung <baby_ash@naver.com>
2023-06-14 23:47:19 +0900

    경로 찾기

---
+++ boj/BOJ_11403.java
@@ -0,0 +1,39 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+ * 경로 찾기
+ * https://www.acmicpc.net/problem/11403
+ */
+public class BOJ_11403 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (map[i][k] == 1 && map[k][j] == 1) {
+                        map[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(map[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
