4a5fd3af61b078d42a19595dcd8ed1f10813be23
SunYoung <baby_ash@naver.com>
2023-07-06 22:25:03 +0900

    감소하는 수

---
+++ boj/BOJ_10171.java
@@ -0,0 +1,13 @@
+package boj;
+
+/**
+ * https://www.acmicpc.net/problem/10171
+ */
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
