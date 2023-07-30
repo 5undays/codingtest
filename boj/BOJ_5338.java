20583da53c79317cecfd64998e25da417f936d53
SunYoung <baby_ash@naver.com>
2023-07-30 22:19:30 +0900

    마이크로소프트 로고

---
+++ boj/BOJ_5338.java
@@ -0,0 +1,15 @@
+package boj;
+
+/**
+ * 마이크로소프트 로고
+ * https://www.acmicpc.net/problem/5338
+ */
+public class BOJ_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
