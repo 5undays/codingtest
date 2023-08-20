fcbe2cf8ef9fa829ca42ace1e365e4ea192b366d
SunYoung <baby_ash@naver.com>
2023-08-20 14:49:32 +0900

    와이버스 부릉부릉

---
+++ boj/BOJ_14645.java
@@ -0,0 +1,11 @@
+package boj;
+
+/**
+ * 와이버스 부릉부릉
+ * https://www.acmicpc.net/problem/14645
+ */
+public class BOJ_14645 {
+    public static void main(String[] args) {
+        System.out.println("비와이");
+    }
+}
