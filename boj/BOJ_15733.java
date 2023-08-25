0b8785633170d381bdabbd974ce9d7d23061d815
SunYoung <baby_ash@naver.com>
2023-08-25 17:35:24 +0900

    나는 누구인가

---
+++ boj/BOJ_15733.java
@@ -0,0 +1,11 @@
+package boj;
+
+/**
+ * 나는 누구인가
+ * https://www.acmicpc.net/problem/15733
+ */
+public class BOJ_15733 {
+    public static void main(String[] args) {
+        System.out.println("I'm Sexy");
+    }
+}
