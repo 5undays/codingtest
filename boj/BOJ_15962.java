fabaf813def499eee8b5d9f2da959406d10acd95
SunYoung <baby_ash@naver.com>
2023-09-05 20:40:59 +0900

    새로운 시작

---
+++ boj/BOJ_15962.java
@@ -0,0 +1,11 @@
+package boj;
+
+/**
+ * 새로운 시작
+ * https://www.acmicpc.net/problem/15962
+ */
+public class BOJ_15962 {
+    public static void main(String[] args) {
+        System.out.println("파이팅!!");
+    }
+}
