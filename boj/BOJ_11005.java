10ca5d354a80f23f1a1a439ecad9b63b3918f976
SunYoung <baby_ash@naver.com>
2023-10-12 21:20:45 +0900

    진법변환

---
+++ boj/BOJ_11005.java
@@ -0,0 +1,10 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_11005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.toString(sc.nextInt(), sc.nextInt()).toUpperCase());
+    }
+}
