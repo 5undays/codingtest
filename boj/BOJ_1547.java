1202d82341e7cc74d68608353573761a08c4ac64
SunYoung <baby_ash@naver.com>
2023-11-05 00:34:20 +0900

    공

--- boj/BOJ_1547.java
+++ boj/BOJ_1547.java
@@ -1,4 +1,21 @@
 package boj;
 
+import java.util.Scanner;
+
 public class BOJ_1547 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int answer = 1;
+        while (m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x == answer) {
+                answer = y;
+            } else if (y == answer) {
+                answer = x;
+            }
+        }
+        System.out.println(answer);
+    }
 }
