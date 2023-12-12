e5e1af5158aa6dfee5c028ad61809c82e6a31952
SunYoung <baby_ash@naver.com>
2023-12-12 22:15:11 +0900

    N번째 큰 수

---
+++ boj/BOJ_2075.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        while (n -- > 1) {
+            pq.poll();
+        }
+
+        System.out.println(pq.poll());
+    }
+}
