1e7766bf3c964d3c90b9b1ea7e22bf46f6a96694
SunYoung <baby_ash@naver.com>
2024-04-15 23:36:45 +0900

    l로 만들기

--- programmers/PGS_181834.java
+++ programmers/PGS_181834.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.stream.Collectors;
+
 /**
  * l로 만들기
  * https://school.programmers.co.kr/learn/courses/30/lessons/181834
@@ -11,11 +13,17 @@ public class PGS_181834 {
 
     public String solution(String myString) {
         StringBuilder answer = new StringBuilder(myString);
-        for (int i =0 ; i < myString.length(); i++) {
+        for (int i = 0; i < myString.length(); i++) {
             if (answer.toString().charAt(i) < 'l') {
                 answer.setCharAt(i, 'l');
             }
         }
         return answer.toString();
+    }
+
+    public String solution1(String myString) {
+        return myString.chars()
+                .mapToObj(i -> Character.toString(Integer.max(i, 'l')))
+                .collect(Collectors.joining());
     }
 }
