8f45a00ca0d350412bb85c3dd16b07d021593cbf
SunYoung <baby_ash@naver.com>
2024-05-08 11:02:51 +0900

    카드 구매하기

---
+++ boj/BOJ_11052.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 카드 구매하기
+ * https://www.acmicpc.net/problem/11052
+ */
+public class BOJ_11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], arr[j] + dp[i - j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
