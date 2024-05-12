39405de73884101f8504add901236909a787bbfa
SunYoung <baby_ash@naver.com>
2024-05-12 21:59:43 +0900

    섬의 개수

--- boj/BOJ_4963.java
+++ boj/BOJ_4963.java
@@ -9,19 +9,20 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/4963
  */
 public class BOJ_4963 {
-    static int[] dx = { 1, -1, 0, 0, -1, -1, 1, 1 };
-    static int[] dy = { 1, -1, 0, 0, 1, -1, 1, -1 };
-
+    static int[] dx = {0, -1, 1, 0, 1, 0, -1, -1, 1};
+    static int[] dy = {0, -1, 1, 1, 0, -1, 0, 1, -1};
+    static int[][] group;
+    static int[][] map;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            int a = sc.nextInt();
             int b = sc.nextInt();
+            int a = sc.nextInt();
             if (a == 0 && b == 0) {
                 break;
             }
-            int[][] group = new int[a][b];
-            int[][] map = new int[a][b];
+            group = new int[a][b];
+            map = new int[a][b];
 
             for (int i = 0; i < a; i++) {
                 for (int j = 0; j < b; j++) {
@@ -33,7 +34,7 @@ public class BOJ_4963 {
             for (int i = 0; i < a; i++) {
                 for (int j = 0; j < b; j++) {
                     if (group[i][j] == 0 && map[i][j] == 1) {
-                        dfs(map, i, j, ++answer, group, a, b);
+                        dfs(i, j, ++answer, a, b);
                     }
                 }
             }
@@ -42,16 +43,14 @@ public class BOJ_4963 {
     }
 
     /**
-     * 
-     * @param map 섬의 맵
-     * @param x 현재 x축 위치
-     * @param y 현재 y축 위치
-     * @param a 현재 그룹 번호
-     * @param group 
-     * @param u x축 범위
-     * @param w 축 범위
+     * 섬의 그룹 번호 설정
+     * @param x     현재 x축 위치
+     * @param y     현재 y축 위치
+     * @param number     현재 그룹 번호
+     * @param u     x축 범위
+     * @param w     축 범위
      */
-    public static void dfs(int[][] map, int x, int y, int a, int[][] group, int u, int w) {
+    public static void dfs(int x, int y, int number, int u, int w) {
         Queue<Pair> q = new LinkedList<Pair>();
         q.add(new Pair(x, y));
         while (!q.isEmpty()) {
@@ -60,23 +59,22 @@ public class BOJ_4963 {
             int py = p.y;
             for (int i = 0; i < dx.length; i++) {
                 int nx = px + dx[i];
-                int ny = py + dy[y];
+                int ny = py + dy[i];
                 if (nx >= 0 && nx < u && ny >= 0 && ny < w && group[nx][ny] == 0 && map[nx][ny] == 1) {
                     q.add(new Pair(nx, ny));
-                    group[nx][ny] = a;
+                    group[nx][ny] = number;
                 }
             }
         }
+    }
+    static class Pair {
+        int x;
+        int y;
 
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
 
-class Pair {
-    int x;
-    int y;
-
-    Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
