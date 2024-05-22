3e1a3813de9ba7075684b03eff64c2889cdf2582
SunYoung <baby_ash@naver.com>
2024-05-22 19:29:23 +0900

    골드바흐의 추측

--- boj/BOJ_6588.java
+++ boj/BOJ_6588.java
@@ -13,27 +13,36 @@ public class BOJ_6588 {
         boolean check[] = new boolean[1000001];
         check[0] = check[1] = true;
         ArrayList<Integer> arr = new ArrayList<>();
-        for (int i = 2; i * i < 1000000; i++) {
-            if (check[i] == false) {
+        for (int i = 2; i < Math.sqrt(1000000); i++) {
+            if (!check[i]) {
                 arr.add(i);
-                for (int j = i+i; j < 1000000; j += i) {
+                for (int j = i * i; j < 1000000; j += i) {
                     check[j] = true;
                 }
             }
         }
 
+        boolean notFound = true;
+
+        StringBuilder sb = new StringBuilder();
         while (true) {
             int n = sc.nextInt();
             if (n == 0)
                 break;
-            for (int i = 1; i < arr.size(); i++) {
+            for (int i = 1; i < n / 2 + 1; i++) {
                 int p = arr.get(i);
-                if (check[n - p] == false) {
-                    System.out.println(n + " = " + p + " + " + (n - p));
+                if (!check[n - p] && !check[p]) {
+                    sb.append(n + " = " + p + " + " + (n - p)).append("\n");
+                    notFound = false;
                     break;
                 }
             }
+        }
 
+        if (notFound) {
+            System.out.println("Goldbach's conjecture is wrong.");
+        } else {
+            System.out.println(sb);
         }
     }
 }
