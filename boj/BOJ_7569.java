8fafdb84d4cb1c894d877474abd068d567235c6b
SunYoung <baby_ash@naver.com>
2024-05-30 14:20:19 +0900

    토마토

--- boj/BOJ_7569.java
+++ boj/BOJ_7569.java
@@ -19,6 +19,7 @@ public class BOJ_7569 {
     static int m;
     static int n;
     static int h;
+    static boolean[][][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,59 +30,54 @@ public class BOJ_7569 {
         box = new int[n][m][h];
         Queue<int[]> q = new LinkedList<>();
 
-        boolean check = true;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                box[i][j][0] = Integer.parseInt(st.nextToken());
-                if (box[i][j][0] == 1) {
-                    q.add(new int[] {i, j, 0});
-                } else if (box[i][j][0] == 0) {
-                    check = false;
-                }
-            }
-        }
-
-        if (check) {
-            int answer = 0;
-            while (!q.isEmpty()) {
-                int[] xy = q.poll();
-                int x = xy[0];
-                int y = xy[1];
-                int z = xy[2];
-
-                for (int i = 0; i < dx.length; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    int nz = z + dz[i];
-                    if (nx >= 0 && ny >= 0 && nz >= 0 && box[nx][ny][nz] == 1) {
-                        box[nx][ny][nz] = 1;
-                        q.add(new int[] {nx, ny, nz});
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < m; k++) {
+                    box[i][j][k] = Integer.parseInt(st.nextToken());
+                    if (box[i][j][k] == 1) {
+                        q.add(new int[] {i, j, k});
                     }
-                    answer++;
                 }
             }
-
-            if (validate()) {
-                System.out.println(answer);
-            } else {
-                System.out.println(-1);
-            }
-        } else {
-            System.out.println(0);
         }
+
+        int answer = 0;
+        while (!q.isEmpty()) {
+            int[] xy = q.poll();
+            int z = xy[0];
+            int x = xy[1];
+            int y = xy[2];
+
+            for (int i = 0; i < dx.length; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                int nz = z + dz[i];
+                if (nx >= 0 && ny >= 0 && nz >= 0
+                        && nx < n && ny < m && nz < h) {
+                    if (box[nz][nx][ny] == 0) {
+                        box[nz][nx][ny] = box[z][x][y] + 1;
+                        q.add(new int[] {nz, nx, ny});
+                    }
+                }
+            }
+        }
+
+        System.out.println(validate());
     }
 
-    private static boolean validate() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                for (int k = 0; k < h; k++) {
+    private static int validate() {
+        int result = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
                     if (box[i][j][k] == 0) {
-                        return false;
+                        return -1;
                     }
+                    result = Math.max(result, box[i][j][k]);
                 }
             }
         }
-        return true;
+        return result - 1;
     }
 }
