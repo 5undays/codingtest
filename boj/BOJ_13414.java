211ee677b03253d3e99e36a4089723a0f72eab78
SunYoung <baby_ash@naver.com>
2024-06-05 23:04:46 +0900

    수강신청

---
+++ boj/BOJ_13414.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 수강신청
+ * https://www.acmicpc.net/problem/13414
+ */
+public class BOJ_13414 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken()); // 과목의 수강 가능 인원
+        int l = Integer.parseInt(st.nextToken()); // 학생들이 버튼을 클릭한 순서를 기록한 대기목록의 길이
+
+        LinkedList<String> arr = new LinkedList<>();
+
+        for (int i = 0; i < l; i++) {
+            String s = br.readLine();
+            arr.remove(s);
+            arr.add(s);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        Iterator iter = arr.iterator();
+
+        while (iter.hasNext() && k-- > 0) {
+            sb.append(iter.next()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
