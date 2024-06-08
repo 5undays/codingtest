92ad7acf9b5f23c182666c4c84896eb599a23494
SunYoung <baby_ash@naver.com>
2024-06-08 17:49:50 +0900

    수강신청

---
+++ boj/BOJ_27433.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+ * 팩토리얼 2
+ * https://www.acmicpc.net/problem/27433
+ */
+public class BOJ_27433 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    private static long factorial(int n) {
+        if (n == 1 || n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+}
