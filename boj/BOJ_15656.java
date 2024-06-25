22cfc3989aecdf461c340243a4e0b8b3a388b541
SunYoung <baby_ash@naver.com>
2024-06-25 22:15:45 +0900

    N과 M (7)

--- boj/BOJ_15656.java
+++ boj/BOJ_15656.java
@@ -8,40 +8,38 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/15656
  */
 public class BOJ_15656 {
-
+    static int n, m;
     static int[] data = new int[10];
-    static int[] a = new int[10];
-
+    static int[] arr = new int[10];
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        data = new int[n];
+        arr = new int[m];
+        visited = new boolean[n];
         for (int i = 0; i < n; i++) {
             data[i] = sc.nextInt();
         }
-        Arrays.sort(data, 0, n);
-        System.out.println(go(0, n, m));
+        Arrays.sort(data);
+        go(0);
+        System.out.println(sb.toString());
     }
 
-    private static StringBuilder go(int index, int n, int m) {
-        if (index == m) {
-            StringBuilder sb1 = new StringBuilder();
-            for (int i = 0; i < m; i++) {
-                sb1.append(data[a[i]]);
-                if (i != m - 1) {
-                    sb1.append(" ");
-                }
+    private static void go(int depth) {
+        if (depth == m) {
+            for (int x : arr) {
+                sb.append(x).append(" ");
             }
-            sb1.append("\n");
-            return sb1;
+            sb.append("\n");
+            return;
         }
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            a[index] = i;
-            sb.append(go(index + 1, n, m));
+            arr[depth] = data[i];
+            go(depth + 1);
         }
-
-        return sb;
     }
 }
