d1c4eae07b7f48a81105cfea7c789b70f26058d6
SunYoung <baby_ash@naver.com>
2024-06-29 09:45:14 +0900

    N과 M (10)

--- boj/BOJ_15664.java
+++ boj/BOJ_15664.java
@@ -1,8 +1,6 @@
 package boj;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -10,95 +8,47 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/15664
  */
 public class BOJ_15664 {
-    static int[] data = new int[10];
+    static int[] data;
+    static StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    static int n,m;
+    static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        data = new int[n];
+        arr = new int[m];
+        visited = new boolean[10001];
         for (int i = 0; i < n; i++) {
             data[i] = sc.nextInt();
         }
-        Arrays.sort(data, 0, n);
-        go(0, 0, n, m);
-        Collections.sort(d);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < d.size(); i++) {
-            if (i == 0 || !d.get(i).equals(d.get(i - 1))) {
-                for (int j = 0; j < m; j++) {
-                    sb.append(d.get(i).get(j));
-                    if (i == m - 1) {
-                        sb.append(" ");
-                    }
-                }
-                sb.append("\n");
-            }
-        }
+        Arrays.sort(data);
+        go(0, 0);
         System.out.println(sb);
     }
 
-    public static int[] a = new int[10];
-    public static ArrayList<Result> d = new ArrayList<>();
-
-    private static void go(int index, int start, int n, int m) {
-        if (index == m) {
-            ArrayList<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < m; i++) {
-                temp.add(data[a[i]]);
+    private static void go(int depth, int start) {
+        if (depth == m) {
+            for (int x : arr) {
+                sb.append(x + " ");
             }
-            d.add(new Result(temp));
+            sb.append("\n");
             return;
         }
 
-        if (index < n) {
-            for (int i = start; i < n; i++) {
-                a[index] = i;
-                go(index + 1, i + 1, n, m);
+        int k = 0;
+        for (int i = start; i < n; i++) {
+            if (k != data[i]) {
+                k = data[i];
+                arr[depth] = data[i];
+                go(depth + 1, i + 1);
             }
         }
     }
+
+
+
 }
 
-class Result implements Comparable<Result> {
-    Integer[] a;
-
-    Result(ArrayList<Integer> a) {
-        this.a = a.toArray(new Integer[a.size()]);
-    }
-
-    int get(int index) {
-        return (int) this.a[index];
-    }
-
-    public boolean equals(Object obj) {
-        if (obj instanceof Result) {
-            Result that = (Result) obj;
-            int n = this.a.length;
-            for (int i = 0; i < n; i++) {
-                if (this.a[i] != that.a[i]) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public int compareTo(Result result) {
-        int n = this.a.length;
-        for (int i = 0; i < n; i++) {
-            if (this.a[i] == result.a[i]) {
-                continue;
-            }
-            if (this.a[i] < result.a[i]) {
-                return -1;
-            }
-            if (this.a[i] > result.a[i]) {
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-}
