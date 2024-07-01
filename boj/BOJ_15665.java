e941eb9b77e861fe82a1f448f50011316fb0e796
SunYoung <baby_ash@naver.com>
2024-07-01 21:22:38 +0900

    N과 M (11)

--- boj/BOJ_15665.java
+++ boj/BOJ_15665.java
@@ -8,49 +8,41 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/15665
  */
 public class BOJ_15665 {
-    static int[] num = new int[10];
-    
+    static int n, m;
+    static int[] arr;
+    static int[] data;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] temp = new int[n];
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        data = new int[m];
         for (int i = 0; i < n; i++) {
-            temp[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         }
-        Arrays.sort(temp);
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && temp[i] == temp[i - 1]) { // 중복 숫자 제거
-                continue;
-            }
-            num[k++] = temp[i];
-        }
-        n = k;
-        System.out.println(go(0, n, m));
+        Arrays.sort(arr);
+        go(0);
+        System.out.println(sb);
     }
 
-    static StringBuilder sb = new StringBuilder();
-    static int[] a = new int[10];
-
-    static StringBuilder go(int index, int n, int m) {
-        if (index == m) {
-            StringBuilder sb = new StringBuilder();
-            for (int i=0; i<m; i++) {
-                sb.append(num[a[i]]);
-                if (i != m-1) sb.append(" ");
+    public static void go(int depth) {
+        if (depth == m) {
+            for (int x : data) {
+                sb.append(x).append(" ");
             }
             sb.append("\n");
-            return sb;
+            return;
         }
-        StringBuilder ans = new StringBuilder();
+
+        int x = 0;
         for (int i=0; i<n; i++) {
-            //if (c[i]) continue;
-            //c[i] = true;
-            a[index] = i;
-            ans.append(go(index+1, n, m));
-            //c[i] = false;
+            if (x != arr[i]) {
+                x = arr[i];
+                data[depth] = arr[i];
+                go(depth + 1);
+            }
         }
-        return ans;
     } 
 }
