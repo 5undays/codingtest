763320c08dfed16a378885546b332c3ac0468c1c
SunYoung <baby_ash@naver.com>
2024-07-02 23:37:21 +0900

    N과 M (12)

--- boj/BOJ_15666.java
+++ boj/BOJ_15666.java
@@ -8,55 +8,37 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/15666
  */
 public class BOJ_15666 {
-    static int[] data = new int[10];
+    static int[] data;
     static int[] temp = new int[10];
     static int[] a = new int[10];
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] data = new int[n];
+        n = sc.nextInt();
+        m = sc.nextInt();
+        data = new int[n];
         for (int i = 0; i < n; i++) {
             data[i] = sc.nextInt();
         }
         Arrays.sort(data);
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || data[i] != data[i - 1]) {
-                continue;
-            }
-            temp[k++] = data[i];
-        }
-        System.out.println(go(0, n, m));
+        go(0);
+        System.out.println(sb);
     }
 
-    /**
-     * 
-     * @param index
-     * @param n
-     * @param m
-     * @return
-     */
-    private static StringBuilder go(int index, int n, int m) {
-        if (index == m) {
-            StringBuilder sb = new StringBuilder();
+    private static void go(int depth) {
+        if (depth == m) {
             for (int i = 0; i < m; i++) {
-                sb.append(temp[a[i]]);
-                if (i != m - 1)
-                    sb.append(" ");
+                sb.append(temp[a[i]]).append(" ");
             }
             sb.append("\n");
-            return sb;
+            return;
         }
 
-        StringBuilder sb1 = new StringBuilder();
-        if (index < n) {
-            for (int i = 0; i < n; i++) {
-                a[index] = i;
-                sb1.append(go(index + 1, n, m));
-            }
+        for (int i = 0; i < n; i++) {
+            a[depth] = i;
+            go(depth + 1);
         }
-        return sb1;
     }
 }
