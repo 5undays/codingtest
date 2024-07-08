fa6c3af8850f906eb78fd9cca4ac1d6e2958cea3
SunYoung <baby_ash@naver.com>
2024-07-08 18:58:05 +0900

    가운데를 말해요

--- boj/BOJ_1655.java
+++ boj/BOJ_1655.java
@@ -3,6 +3,7 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 /**
@@ -13,22 +14,32 @@ public class BOJ_1655 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
-        int k = Integer.parseInt(br.readLine());
-        sb.append(k).append("\n");
-        pq.add(k);
 
-        for (int i = 1; i < n; i++) {
-            int q = Integer.parseInt(br.readLine());
-            if (pq.peek() < q) {
-                sb.append(pq.peek());
+        for (int i = 0 ; i < n; i++) {
+            int k = Integer.parseInt(br.readLine());
+
+            if (max.isEmpty()) {
+                max.add(k);
             } else {
-                pq.poll();
-                sb.append(q);
-                pq.add(q);
+                if (max.size() > min.size()) {
+                    min.add(k);
+                } else {
+                    max.add(k);
+                }
+
+                int maxTop = max.peek();
+                int minTop = min.peek();
+
+                if (maxTop > minTop) {
+                    max.poll(); min.poll();
+                    max.add(minTop);
+                    min.add(maxTop);
+                }
             }
-            sb.append("\n");
+            sb.append(max.peek()).append("\n");
         }
         System.out.println(sb);
     }
