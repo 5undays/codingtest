867db198a3a1e0908070188a99328ff7fdb359f9
SunYoung <baby_ash@naver.com>
2024-07-09 21:54:26 +0900

    운동

--- boj/BOJ_1956.java
+++ boj/BOJ_1956.java
@@ -3,7 +3,6 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -11,6 +10,8 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/1956
  */
 public class BOJ_1956 {
+    public static final int INF = 4000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,7 +21,7 @@ public class BOJ_1956 {
         int[][] arr = new int[v + 1][v + 1];
         for (int i = 1; i <= v; i++) {
             for (int j = 1; j <= v; j++) {
-                if (i != j) arr[i][j] = 10001;
+                arr[i][j] = INF;
             }
         }
 
@@ -34,21 +35,27 @@ public class BOJ_1956 {
 
         for (int i = 1; i <= v; i++) {
             for (int j = 1; j <= v; j++) {
+                if (i == j) continue;
                 for (int k = 1; k <= v; k++) {
+                    if (k == j || k == i) continue;
                     arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
                 }
             }
         }
 
-        int answer = Integer.MAX_VALUE;
+        int answer = INF * 2;
         for (int i = 1; i <= v; i++) {
             for (int j = 1; j <= v; j++) {
-                if (i == j) continue;
-                answer = Math.min(answer, arr[i][j] + arr[j][i]);
+                if (arr[i][j] == INF || arr[j][i] == INF) continue;
+                if (i == j) {
+                    answer = Math.min(answer, arr[i][j]);
+                } else {
+                    answer = Math.min(answer, arr[i][j] + arr[j][i]);
+                }
             }
         }
 
-        if (answer == Integer.MAX_VALUE) {
+        if (answer == INF * 2) {
             System.out.println(-1);
         } else {
             System.out.println(answer);
