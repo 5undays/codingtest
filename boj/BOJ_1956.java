271288e3e91ef1b3aad6ea96c0888f2a2c06e557
SunYoung <baby_ash@naver.com>
2024-07-09 21:39:43 +0900

    운동

---
+++ boj/BOJ_1956.java
@@ -0,0 +1,57 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 운동
+ * https://www.acmicpc.net/problem/1956
+ */
+public class BOJ_1956 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[v + 1][v + 1];
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
+                if (i != j) arr[i][j] = 10001;
+            }
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr[a][b] = c;
+        }
+
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
+                for (int k = 1; k <= v; k++) {
+                    arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
+                }
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
+                if (i == j) continue;
+                answer = Math.min(answer, arr[i][j] + arr[j][i]);
+            }
+        }
+
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
+        }
+    }
+}
